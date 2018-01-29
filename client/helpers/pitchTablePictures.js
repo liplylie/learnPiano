0ab3a05e2db3933539e85d1aa831dfb58b09e2cd
Jordan Daniels <decentpianist@gmail.com>
2018-01-29 15:31:14 -0800

    added more photos of notes

--- client/helpers/pitchTablePictures.js
+++ client/helpers/pitchTablePictures.js
@@ -1,15 +1,35 @@
+import F3 from "../static/F3.png"
+import G3 from "../static/G3.jpg"
+import A3 from "../static/A3.jpg"
+import B3 from "../static/B3.jpg"
 import C4 from "../static/C4.gif"
+import C4Bass from "../static/middleCBassClef.gif"
 import D4 from "../static/D4.jpeg"
 import E4 from "../static/E4.png"
 import F4 from "../static/F4.jpg"
 import G4 from "../static/G4.png"
+import A4 from "../static/A4.png"
+import B4 from "../static/B4.jpeg"
+import C5 from "../static/C5.jpg"
+import D5 from "../static/D5.jpg"
+import E5 from "../static/E5.png"
 
 const pitchTablePictures = {
+	F3: F3,
+	G3: G3,
+	A3: A3,
+	B3: B3,
 	C4: C4,
+	C4Bass: C4Bass,
 	D4: D4,
 	E4: E4,
 	F4: F4,
-	G4: G4
+	G4: G4,
+	A4: A4,
+	B4: B4,
+	C5: C5,
+	D5: D5,
+	E5: E5
 
 }
 
