eddf583cab6bb3aac9a9228f751319d58e47ce73
Jordan Daniels <decentpianist@gmail.com>
2018-01-30 15:36:51 -0800

    applied styling to profile table CONTINUE TO LESSON 2;

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -43,34 +43,34 @@ class Profile extends Component{
 					<div className="row" style={{height: "8em"}}>
 					</div>
 					<div className="row">
-						<div className="col-md-4"> 
-							<img className="span3 wow flipInX center"src={this.props.Auth.picture} style={{height: "10em", width:"10em", visibility: "visible", animationName: "flipInX"}}/>
+						<div className="col-md-1"></div>
+						<div className="col-md-3"> 
+							<img className="span3 wow flipInX center" src={this.props.Auth.picture} style={{height: "10em", width:"10em", visibility: "visible", animationName: "flipInX"}}/>
 						</div>
 						<div className="col-md-4"></div>
-						<div className="col-md-4"> <Link to="/lessonOne">lesson one</Link></div>
-						<div className="col-md-4"> <Link to="/lessonTwo">lesson two</Link></div>
+						<div className="col-md-4 text-center"> <Link to="/lessonOne">lesson one</Link><br/><Link to="/lessonTwo">lesson two</Link></div>
 					</div>
-					<div className="row">
-						<div className="col-md-4">
+					<div className="row" style={{paddingTop: "1em", paddingBottom:"1em"}}>
+						<div className="col-md-5 text-center">
 						{this.props.Auth.name}
 						</div>
 					</div>
 					<div className="row">
 						<div className="col-md-1"></div>
-						<div className="col-md-6 ">
-						<table>
+						<div className="col-md-5">
+						<table className="table">
 							<tbody>
-								<tr>
+								<tr style={{backgroundColor: "lightgrey"}}>
 									<th>Lessons</th>
 									<th>Completed</th>
 									<th>Date Finished</th>
 								</tr>
 								{lessonData.map((data, i) =>{
 									return (
-										<tr key={i} > 
-										<th>lesson {i + 1}</th>
-										<th>{data[1].completed ? `completed` : `not completed`} </th>
-										<th>{data[1].time ? new Date(data[1].time).toString().split(" ")[0] + " " + new Date(data[1].time).toString().split(" ")[1] + " " + new Date(data[1].time).toString().split(" ")[2] + ", " +  new Date(data[1].time).toString().split(" ")[3] + " at " + this.formatAMPM(new Date(data[1].time).toString().split(" ")[4]): "n/a"} </th>
+										<tr key={i} className={data[1].completed ? "Complete" : "NotComplete"}> 
+											<th>lesson {i + 1}</th>
+											<th>{data[1].completed ? `completed` : `not completed`} </th>
+											<th>{data[1].time ? new Date(data[1].time).toString().split(" ")[0] + " " + new Date(data[1].time).toString().split(" ")[1] + " " + new Date(data[1].time).toString().split(" ")[2] + ", " +  new Date(data[1].time).toString().split(" ")[3] + " at " + this.formatAMPM(new Date(data[1].time).toString().split(" ")[4]): "n/a"} </th>
 										</tr>
 									)
 								})}
