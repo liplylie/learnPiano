1cbe55bd2f658a7456584933ddf624a30e94d0df
Jordan Daniels <decentpianist@gmail.com>
2018-03-15 11:38:20 -0700

    added content to about page

--- client/components/About.js
+++ client/components/About.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import Piano from "./Piano.js";
 
 class About extends Component {
 	constructor() {
 		super();
 	}
+
+	 showPiano() {
+        document.getElementById("showPiano").style.display = "block";
+        document.getElementById("showPianoButton").style.display = "none";
+    }
+
+    hidePiano() {
+        document.getElementById("showPiano").style.display = "none";
+        document.getElementById("showPianoButton").style.display = "block";
+    }
 
 	render() {
 		return (
@@ -27,7 +38,42 @@ class About extends Component {
 					className="effect8 wow fadeIn"
 				>
 					<div className="row" style={{ height: "7em" }} />
-					<div className="col-md-12">About</div>
+					<div className="row">
+					<div className="col-md-12 text-center" style={{fontSize: "2em"}}>
+						<h1>Learn Piano Fun</h1>
+						<p>Learn Piano Fun is a website for interactively learning how to play the piano</p>
+						<p>There is no need for MIDI controllers or other devices that connect to the computer</p>
+						<p>All that you need is a piano or piano app to play the piano. Make sure that the volume of the piano can reach the microphone of your computer or laptop</p>
+						<p>Even if you do not own a piano, that's ok! With the built in piano, you can still play through the app. See it here:</p>
+						<div
+                        id="showPianoButton"
+                        style={{ cursor: "pointer" }}
+                        onClick={() => this.showPiano()}
+                    >
+                        <img
+                            className="wow rollIn"
+                            style={{
+                                height: "4em",
+                                width: "4em",
+                                margin: ".5em"
+                            }}
+                            src={require("../static/pianoKeys.png")}
+                        />
+                        <p className="wow flipInX center"> Open Piano</p>
+                    </div>
+                    <div id="showPiano" style={{ display: "none" }}>
+                        <Piano />
+                        <p
+                            onClick={() => this.hidePiano()}
+                            style={{ cursor: "pointer" }}
+                        >
+                            {" "}
+                            hide
+                        </p>
+                    </div>
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/JMEqOJtxNtI" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+					</div>
+					</div>
 				</div>
 			</div>
 		);
